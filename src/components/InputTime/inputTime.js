3e6a3995766a904d70ea36ce8c379f915e7f2752
Mudassir Raza <razamudassir912@gmail.com>
2021-03-31 10:42:24 +0500

    dayMangement inProcess

--- src/components/InputTime/inputTime.js
+++ src/components/InputTime/inputTime.js
@@ -1,39 +1,28 @@
 
-// import * as React from 'react'
-// import { ViewStyle, TouchableOpacity, TextStyle, Image } from 'react-native'
-// import { Text } from '..'
-// import { palette } from '../../theme/palette'
-// import { spacing } from '../../theme'
-// import assets from '../../../assets/index'
-// import DateTimePickerModal from 'react-native-modal-datetime-picker'
-// import moment from 'moment'
+import * as React from 'react'
+import { ViewStyle, TouchableOpacity, TextStyle, Image, Text, Platform } from 'react-native'
+import DateTimePickerModal from 'react-native-modal-datetime-picker'
+import moment from 'moment'
+import { fontStyles } from '../../constants/fontStyles'
 
-// export interface InputTimerops {
-//   value?: Date
-//   onTimeChange: (date) => void
-//   placeHolderText?: string
-//   style?: ViewStyle
-//   textStyle?: TextStyle
-// }
 
-// const localTextStyle = {
-//   fontSize: 14,
-//   fontFamily: 'Poppins-SemiBold',
-//   color: palette.black,
-// } as TextStyle
-// const BASE_VIEW: ViewStyle = {
+const defaulLableStyle = {
+    paddingVertical: 5,
+    maxWidth: '100%',
+    alignSelf: 'flex-start'
+}
+const BASE_VIEW = {
 //   borderRadius: 12,
 //   justifyContent: 'space-between',
 //   alignItems: 'center',
 //   alignSelf: 'stretch',
 //   flexDirection: 'row',
 //   minHeight: 48,
-//   backgroundColor: palette.white,
+//   backgroundColor: "#fff",
 //   borderWidth: 1,
 //   paddingVertical: 14,
-//   paddingHorizontal: spacing[5],
-//   marginVertical: spacing[3],
-//   borderColor: palette.borderColor,
+//   paddingHorizontal: 15,
+//   marginVertical: 10,
 //   shadowColor: 'rgba(0, 0, 0, 0.15)',
 //   shadowOffset: {
 //     width: 1,
@@ -41,48 +30,60 @@
 //   },
 //   shadowOpacity: 0.2,
 //   elevation: 4,
-// }
+    backgroundColor: "#F1F2FA",
+    padding: Platform.OS == "ios" ? 20 : 5,
+    alignSelf: 'center',
+    width: '100%',
+    borderRadius: 10,
+}
 
-// /**
-//  * Stateless functional component for your needs
-//  *
-//  * Component description here for TypeScript tips.
-//  */
-// export function InputTime(props: InputTimerops) {
-//   // grab the props
-//   const { placeHolderText, style, value = '', textStyle, ...rest } = props
+/**
+ * Stateless functional component for your needs
+ *
+ * Component description here for TypeScript tips.
+ */
+export function InputTime({ 
+    placeHolderText, 
+    style, 
+    value = '', 
+    textStyle, 
+    onTimeChange = () => {},
+    label = "Label",
+    ...rest }) {
+  // grab the props
+  
 
-//   const [selectedValue, setSelectedValue] = React.useState(value ? moment(value).format('HH:mm') : '')
-//   const [isTimePickerVisible, setTimePickerVisibility] = React.useState(false)
+  const [selectedValue, setSelectedValue] = React.useState(value ? moment(value).format('HH:mm') : '')
+  const [isTimePickerVisible, setTimePickerVisibility] = React.useState(false)
 
-//   const showTimePicker = () => {
-//     setTimePickerVisibility(true)
-//   }
+  const showTimePicker = () => {
+    setTimePickerVisibility(true)
+  }
 
-//   const hideTimePicker = () => {
-//     setTimePickerVisibility(false)
-//   }
-//   const content = (
-//     <Text text={selectedValue.toString() || placeHolderText} style={[localTextStyle, textStyle]} />
-//   )
-//   // main Render
-//   return (
-//     <>
-//       <TouchableOpacity style={[BASE_VIEW, style]} {...rest} onPress={() => showTimePicker()}>
-//         {content}
-//         <Image source={assets.clock} />
-//       </TouchableOpacity>
-//       {/* Modal on select input click */}
-//       <DateTimePickerModal
-//         isVisible={isTimePickerVisible}
-//         mode="time"
-//         onConfirm={time => {
-//           setSelectedValue(moment(time).format('HH:mm'))
-//           props.onTimeChange(time)
-//           hideTimePicker()
-//         }}
-//         onCancel={hideTimePicker}
-//       />
-//     </>
-//   )
-// }
+  const hideTimePicker = () => {
+    setTimePickerVisibility(false)
+  }
+  const content = (
+    <Text style={[fontStyles.ProximaSemiBoldSmall, textStyle]}>{selectedValue.toString() || placeHolderText}</Text>
+  )
+  // main Render
+  return (
+    <>
+         <Text style={[defaulLableStyle,  fontStyles.ProximaRegularP2]}>{label}</Text>
+      <TouchableOpacity style={[BASE_VIEW, style]} {...rest} onPress={() => showTimePicker()}>
+        {content}
+      </TouchableOpacity>
+      {/* Modal on select input click */}
+      <DateTimePickerModal
+        isVisible={isTimePickerVisible}
+        mode="time"
+        onConfirm={time => {
+          setSelectedValue(moment(time).format('HH:mm'))
+          onTimeChange(time)
+          hideTimePicker()
+        }}
+        onCancel={hideTimePicker}
+      />
+    </>
+  )
+}
--- src/screens/StoreManagment/dayMangment.js
+++ src/screens/StoreManagment/dayMangment.js
@@ -4,19 +4,40 @@ import { NotificationSettingList } from '../../components/List/notificationSetti
 import CustomModal from '../../components/Modal/index'
 import Header from '../../components/Header'
 import { fontStyles } from '../../constants/fontStyles'
-
+import moment from 'moment'
+import { InputTime } from '../../components/InputTime/inputTime'
 const DayManagement = ({
     route,
     navigation, 
 }) => {
     const {date} = route.params
-    console.log()
+    var month = moment(date.dateString).format("MMMM")
+    var dateCustomize = `${month} ${date.day}, ${date.year}`
     return(
-        <View>
+        <View style={{flex: 1, backgroundColor: '#fff'}}>
             <Header leftIconName="arrow-back" leftButtonPress={() => navigation.goBack()} />
-            <NotificationSettingList titleStyle={fontStyles.ProximaBoldH2} title={date.dateString}  />
+            <View style={styles.blockContainer}>
+                <NotificationSettingList titleStyle={fontStyles.ProximaBoldH2} title={dateCustomize}  />
+            </View>
+
+            <View style={styles.blockContainer}>
+                <InputTime label="Opening Time" placeHolderText="HH:mm" onTimeChange={() => {}} />
+            </View>
+            <View style={styles.blockContainer}>
+                <InputTime label="Closing Time" placeHolderText="HH:mm" onTimeChange={() => {}} />
+            </View>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    blockContainer: {
+        paddingVertical: 5,
+        paddingHorizontal: 15
+    },
+    itemContainer: {
+        paddingVertical: 5,
+    },
+})
 
 export default DayManagement

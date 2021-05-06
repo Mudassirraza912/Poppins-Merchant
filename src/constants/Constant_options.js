d3e2031fc4f5346f46beb040891f2f124bd5edaf
Mudassir Raza <mudassirraza@MudassirsMacbookPro.local>
2021-05-07 00:10:53 +0500

    setup s3 bucket

---
+++ src/constants/Constant_options.js
@@ -0,0 +1,11 @@
+
+export const S3_BUCKET_OPTIONS = {
+    keyPrefix: 'images/', // Ex. myuploads/
+    bucket: 'poppins', // Ex. aboutreact
+    region: 'us-east-1', // Ex. ap-south-1
+    accessKey: 'AKIARHLUHGFBQDMO55HY',
+    // Ex. AKIH73GS7S7C53M46OQ
+    secretKey: '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',
+    // Ex. Pt/2hdyro977ejd/h2u8n939nh89nfdnf8hd8f8fd
+    successActionStatus: 201,
+  }
